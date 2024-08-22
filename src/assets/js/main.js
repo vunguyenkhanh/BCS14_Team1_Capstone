03030f0071d410888170181fe4a973edc4751651
Vũ Nguyễn Khánh <vunguyenkhanh.6599@gmail.com>
2024-08-22 14:59:35 +0700

    feat: add Banner section and update/fix Header layout

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,3 +6,18 @@ document
     var dropdownArrow = document.getElementById('dropdownArrow');
     dropdownArrow.classList.toggle('rotate-180');
   });
+
+// navBarDropDown
+  document
+    .getElementById('navBarDropDown')
+    .addEventListener('click', function () {
+      const navBarPath = document.getElementById('navBarPath');
+      const originalPath = 'M1 1h15M1 7h15M1 13h15';
+      const newPath = 'M6 18 17.94 6M18 18 6.06 6';
+
+      if (navBarPath.getAttribute('d') === originalPath) {
+        navBarPath.setAttribute('d', newPath);
+      } else {
+        navBarPath.setAttribute('d', originalPath);
+      }
+    });
