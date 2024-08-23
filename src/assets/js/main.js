f1f32bf07d71678a6099aca9827b19f14e89afe8
Vũ Nguyễn Khánh <vunguyenkhanh.6599@gmail.com>
2024-08-23 13:27:02 +0700

    chore: Refactor header scroll behavior and styles

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,15 +1,21 @@
 window.addEventListener('scroll', function () {
   const header = document.querySelector('header');
   if (window.scrollY > 10) {
-    header.classList.add('bg-white/80', 'border-gray-200', 'backdrop-blur-lg', 'py-2');
-    header.classList.remove('py-5');
+    header.classList.add(
+      'bg-white/80',
+      'border-gray-200',
+      'backdrop-blur-lg',
+      'py-2',
+    );
+    header.classList.remove('border-transparent', 'py-5');
   } else {
     header.classList.remove(
       'bg-white/80',
       'border-gray-200',
       'backdrop-blur-lg',
+      'py-2',
     );
-    header.classList.add('py-5');
+    header.classList.add('border-transparent', 'py-5');
   }
 });
 
