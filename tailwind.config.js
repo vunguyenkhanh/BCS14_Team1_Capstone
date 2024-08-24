c6ae3ca3e8811ce920d2cea9029cbdedd72ac166
Vũ Nguyễn Khánh <vunguyenkhanh.6599@gmail.com>
2024-08-24 09:41:12 +0700

    chore: update Home page with new Sections and update tailwind.config.js with new theme extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,19 @@ module.exports = {
         'gradient-radial':
           'radial-gradient(rgb(199, 210, 254), rgba(199, 210, 254, 0))',
       },
+      colors: {
+        'indigo-600': 'rgb(79, 70, 229)',
+        'indigo-800': 'rgb(55 48 163)',
+      },
+      textColor: {
+        'purple-600': 'rgb(147, 51, 234)',
+      },
+      backgroundColor: {
+        'purple-100': 'rgb(243 232 255)',
+      },
+      borderColor: {
+        'purple-200': 'rgb(233 213 255)',
+      },
     },
   },
   plugins: [require('flowbite/plugin')],
